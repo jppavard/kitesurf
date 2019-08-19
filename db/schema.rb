e9daee61a1cb5b3dcfb3e42cbc2bfd72134db350
jerome couriol <info@jcjp.fr>
2019-08-19 16:37:30 +0200

    migration model reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_142915) do
+ActiveRecord::Schema.define(version: 2019_08_19_143621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2019_08_19_142915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_equipment_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "equipment_id"
+    t.bigint "user_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "paid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["equipment_id"], name: "index_reservations_on_equipment_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +62,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_142915) do
   end
 
   add_foreign_key "equipment", "users"
+  add_foreign_key "reservations", "equipment"
+  add_foreign_key "reservations", "users"
 end
