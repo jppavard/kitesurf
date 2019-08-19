c2dd1158cfe0efa07cc1279be46473db03804f63
Pavard <pavard.pub+gitKraken@gmail.com>
2019-08-19 18:31:35 +0200

    controllers reservations et equipments à tester avec db seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :equipments
+  resources :equipments do
+    resources :reservations, only: %i[index show new create destroy]
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
