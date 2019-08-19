8c9468c9a18a258f71d2416f610e4abbda9c74f3
jerome couriol <info@jcjp.fr>
2019-08-19 18:32:56 +0200

    add validations on user + modification equipment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
+has_many :reservations
+has_many :equipments, through: :reservations
+has_many :owned_equipments, class_name: 'Equipment'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :avatar, PhotoUploader
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :avatar, presence: true
+  validates :email, presence: true, uniqueness: true
 end
