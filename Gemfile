3634348ae65bc20ccfc6a4eb2e2cc4e1570205cb
Guillaume <guillaumecromer@gmail.com>
2019-08-22 11:35:54 +0200

    form-horizontal-bar in progress

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.3'
 gem 'redis'
+gem 'bootstrap-slider-rails'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+
+
+
+import Slider from "bootstrap-slider";
+
+console.log('toto')
+var slider = new Slider('#ex1', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
